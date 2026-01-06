b2748a969b4c63af2b3d9c616d2204a2455e754a
Adrian von Auenmüller <nava.210704@gmail.com>
2026-01-06 15:52:44 +0100

    2. versuch config anzupassen um vercel zum laufen zu bekommen

--- Test123/constants/config.ts
+++ Test123/constants/config.ts
@@ -1,2 +1,2 @@
 // Test123/constants/config.ts
-export const API_BASE_URL = 'https://famstore-backend-2hyoar8vy-adrian-von-auenmullers-projects.vercel.app'; 
+export const API_BASE_URL = 'https://famstore-backend.vercel.app/'; 
