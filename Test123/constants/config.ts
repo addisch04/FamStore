82f4f120f9c68e394db905719cae84929b8237ae
Adrian von Auenmüller <nava.210704@gmail.com>
2026-01-06 15:51:14 +0100

    versuch config url anpassen

--- Test123/constants/config.ts
+++ Test123/constants/config.ts
@@ -1,2 +1,2 @@
 // Test123/constants/config.ts
-export const API_BASE_URL = 'https://famstore-backend-2hyoar8vy-adrian-von-auenmullers-projects.vercel.app/api/pantry'; 
+export const API_BASE_URL = 'https://famstore-backend-2hyoar8vy-adrian-von-auenmullers-projects.vercel.app'; 
